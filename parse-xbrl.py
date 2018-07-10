40285cb4a64db4bb67612234b3b4e9f66396cd4e
u6k <u6k.apps@gmail.com>
2018-07-10 17:25:11 +0900

    add sample script

--- parse-xbrl.py
+++ parse-xbrl.py
@@ -6,3 +6,14 @@ end = datetime(2017, 6, 23)
 ufos = DataReader('7203', 'ufo', start, end, fetch_xbrl=False)
 print(ufos)
 
+
+
+from UfoDataReader.util.parser import UfoXBRLParser
+
+ufoparser = UfoXBRLParser()
+xbrl = ufoparser.parse('sample.xbrl')
+dei = ufoparser.parseDEI(xbrl)
+print(dei.__dict__)
+gaap = ufoparser.parseGAAP(xbrl)
+print(gaap.__dict__)
+
